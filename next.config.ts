fcbc8c1bb4f76efa87b0a2e958d589f3821cb6c1
ytiK0 <eeeee@eee.eee>
2025-03-28 15:29:58 +0300

    enable case cast for css module class names

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    const rules = config.module.rules
+      // @ts-ignore
+      .find((rule) => typeof rule.oneOf === 'object').oneOf.filter((rule) => Array.isArray(rule.use));
+    // @ts-ignore
+    rules.forEach((rule) => {
+      // @ts-ignore
+      rule.use.forEach((moduleLoader) => {
+        if (
+          moduleLoader.loader !== undefined
+          && moduleLoader.loader.includes('css-loader')
+          && typeof moduleLoader.options.modules === 'object'
+        ) {
+          moduleLoader.options = {
+            ...moduleLoader.options,
+            modules: {
+              ...moduleLoader.options.modules,
+              // This is where we allow camelCase class names
+              exportLocalsConvention: "dashes"
+            }
+          };
+        }
+      });
+    });
+
+    return config;
+  }
 };
 
 export default nextConfig;
